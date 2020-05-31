00ffc67ceb75ce85f385e8959fe28e48fc7f6ba9
twangerine <daveh789@gmail.com>
2020-05-30 20:09:57 -0400

    Add 3d plotting for debugging

--- cart2sphere.py
+++ cart2sphere.py
@@ -1,4 +1,18 @@
 # Cartesian to Spherical Co-ordinates function
 
-import math
+from math import acos, asin, atan, atan2
+from math import sin, cos, tan
+from math import pi, sqrt
+
+def cart2sphere(x,y,z):
+    r = sqrt(x*x+y*y+z*z)
+    theta = atan2(y, x)
+    phi = acos(z/r)
+    return r, theta, phi
+
+def sphere2cart(r,theta,phi):
+    x = r * sin(phi) * cos(theta)
+    y = r * sin(phi) * sin(theta)
+    z = r * cos(phi)
+    return x, y, z
 
